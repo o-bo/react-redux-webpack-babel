efd3f965271f0d74dafa4dc7af3803cb370f5a5f
Olivier Berho <olivier@kasual.biz>
2015-12-19 15:53:28 +0100

    [+] Seed admin user with role.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create email: "admin@rrwb.io", password: "pleaseplease", password_confirmation: "pleaseplease"
+admin.add_role :admin
