d9a6758d449da6d73f0a37135cda4800889e76ab
Olivier Berho <olivier@kasual.biz>
2015-12-27 18:09:05 +0100

    [+] Add dependency to material-ui.

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello, World!</h1>
+
+        <RaisedButton label="Primary" primary={true} />
       </div>
     );
   }
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 React.render(
   <App />,
